5022df040b4c8f0dd224f4ed51762ef96c5359ea
Sankalp Mukim <sankalpmukim@gmail.com>
2022-04-24 22:35:02 +0530

    Removing tailwind template

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import Home from './pages/Home';
-
-export default function App() {
-  return <Home />;
+function App() {
+  return <div>Hello World</div>;
 }
+
+export default App;
