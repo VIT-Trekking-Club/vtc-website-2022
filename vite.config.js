2f670764d6ed575b5c35dac7fe6ca9350d45d99f
Sankalp Mukim <sankalpmukim@gmail.com>
2022-04-26 22:31:37 +0530

    'deploy'

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-const path = require('path');
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vtc-website-2022/',
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, '/src') }],
-  },
-  plugins: [reactRefresh()],
+  plugins: [react()],
 });
