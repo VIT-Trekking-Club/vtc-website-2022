18d9a1126617a67a6e484a4239362f4816f0bb44
Sankalp Mukim <sankalpmukim@gmail.com>
2022-05-29 23:45:21 +0530

    Gallery component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import GalleryComponent from "./components/Gallery";
 import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Home />
+      <GalleryComponent />
       <div>{`Hello World (Just for scroll testing)`}</div>
     </>
   );
